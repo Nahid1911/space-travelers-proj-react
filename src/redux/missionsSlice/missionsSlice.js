830a1c47b7536f10b023076461704c4b7917e150
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-27 20:55:25 +0300

    Fetch data from the Missions endpoint

--- src/redux/missionsSlice/missionsSlice.js
+++ src/redux/missionsSlice/missionsSlice.js
@@ -1,5 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const MISSIONS_API_URL = 'https://api.spacexdata.com/v3/missions';
+export const FetchMissions = createAsyncThunk(
+  'redux/missions/missions.js',
+  async (_, thunkAPI) => {
+    const response = await fetch(MISSIONS_API_URL)
+      .catch((error) => thunkAPI.rejectWithValue(error));
+    return response.json();
+  },
+);
+
 const initialState = {
   missions: 'Intial state',
 };
