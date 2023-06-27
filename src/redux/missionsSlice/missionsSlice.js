838ae993871cc2a687a60f67bdfbb0890ea80b5b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-27 20:59:09 +0300

    Intialize isLoading with boolean

--- src/redux/missionsSlice/missionsSlice.js
+++ src/redux/missionsSlice/missionsSlice.js
@@ -5,13 +5,14 @@ export const FetchMissions = createAsyncThunk(
   'redux/missions/missions.js',
   async (_, thunkAPI) => {
     const response = await fetch(MISSIONS_API_URL)
-      .catch((error) => thunkAPI.rejectWithValue(error));
+      .catch((error) => thunkAPI.rejectWithValue('An error occurred while fetching the data'));
     return response.json();
   },
 );
 
 const initialState = {
   missions: 'Intial state',
+  isLoading: false,
 };
 
 const missionSlice = createSlice({
