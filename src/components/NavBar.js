368ca2308a19fa07add696071adf8931137a7b3f
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-27 11:47:31 +0300

    remove commented code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,16 +51,6 @@ const NavBar = () => {
       </Nav>
     </Stack>
   );
-
-/*   <div className="NavContainer">
-    <div>Logo</div>
-    <ul>
-      <li><Link to="/">Rocket</Link></li>
-      <li><Link to="/Mission">Mission</Link></li>
-      <li><Link to="/Dragon">Dragon</Link></li>
-      <li><Link to="/MyProfile">My Profile</Link></li>
-    </ul>
-  </div> */
 };
 
 export default NavBar;
