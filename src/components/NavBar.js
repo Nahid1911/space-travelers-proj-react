f30e7322ee5ef97e844d612e022168d70794a74c
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-27 12:02:10 +0300

    code refactor of navigation section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,33 +7,18 @@ import NavStyles from './NavBar.module.css';
 
 const NavBar = () => {
   const location = useLocation();
-  const rocketLinkStyle = (pathname) => {
-    if (pathname === '/rockets' || pathname === '/' || pathname === '') {
-      return `${NavStyles.linksStyles} ${NavStyles.mr1} ${NavStyles.active}`;
+
+  const getNavLinkStyle = (pathname, style) => {
+    if (location.pathname === pathname) {
+      return `${style} ${NavStyles.active}`;
     }
-    return `${NavStyles.linksStyles} ${NavStyles.mr1}`;
+    return style;
   };
 
-  const missionsLinkStyle = (pathname) => {
-    if (pathname === '/mission') {
-      return `${NavStyles.linksStyles} ${NavStyles.mr1} ${NavStyles.active}`;
-    }
-    return `${NavStyles.linksStyles} ${NavStyles.mr1}`;
-  };
-
-  const dragonLinkStyle = (pathname) => {
-    if (pathname === '/dragon') {
-      return `${NavStyles.linksStyles} ${NavStyles.borderRight} ${NavStyles.active}`;
-    }
-    return `${NavStyles.linksStyles} ${NavStyles.borderRight}`;
-  };
-
-  const myProfileLinkStyle = (pathname) => {
-    if (pathname === '/myprofile') {
-      return `${NavStyles.linksStyles} ${NavStyles.ml1} ${NavStyles.active}`;
-    }
-    return `${NavStyles.linksStyles} ${NavStyles.ml1}`;
-  };
+  const rocketLinkStyle = getNavLinkStyle('/', `${NavStyles.linksStyles} ${NavStyles.mr1}`);
+  const missionsLinkStyle = getNavLinkStyle('/mission', `${NavStyles.linksStyles} ${NavStyles.mr1}`);
+  const dragonLinkStyle = getNavLinkStyle('/dragon', `${NavStyles.linksStyles} ${NavStyles.borderRight}`);
+  const myProfileLinkStyle = getNavLinkStyle('/myprofile', `${NavStyles.linksStyles} ${NavStyles.ml1}`);
 
   return (
     <Stack className={NavStyles.navbarContainer}>
@@ -44,10 +29,10 @@ const NavBar = () => {
         <h1>Space Travelers&apos; HUB</h1>
       </div>
       <Nav activeKey="/" className={NavStyles.linksContainer}>
-        <Nav.Item><Nav.Link as={NavLink} to="/" exact="true" className={rocketLinkStyle(location.pathname)}>Rockets </Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link as={NavLink} to="/mission" className={missionsLinkStyle(location.pathname)}>Missions</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link as={NavLink} to="/dragon" className={dragonLinkStyle(location.pathname)}>Dragon</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link as={NavLink} to="/myprofile" className={myProfileLinkStyle(location.pathname)}>My Profile</Nav.Link></Nav.Item>
+        <Nav.Item><Nav.Link as={NavLink} to="/" exact="true" className={rocketLinkStyle}>Rockets </Nav.Link></Nav.Item>
+        <Nav.Item><Nav.Link as={NavLink} to="/mission" className={missionsLinkStyle}>Missions</Nav.Link></Nav.Item>
+        <Nav.Item><Nav.Link as={NavLink} to="/dragon" className={dragonLinkStyle}>Dragon</Nav.Link></Nav.Item>
+        <Nav.Item><Nav.Link as={NavLink} to="/myprofile" className={myProfileLinkStyle}>My Profile</Nav.Link></Nav.Item>
       </Nav>
     </Stack>
   );
