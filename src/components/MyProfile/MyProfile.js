164c596f9393d72ab47fbd75b937e88263ceeb80
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-28 15:04:59 +0300

    Create basic structure of my proifle page

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import { Table } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import MyProfileStyle from './MyProfile.module.css';
+import { leavingMission } from '../../redux/missionsSlice/missionsSlice';
 
-const MyProfile = () => (
-  <div>Put My Profile information here</div>
-);
+const MyProfile = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+  const joinedmissions = missions.filter((mission) => mission.reserved === true);
+  return (
+    <div className="profile-container mx-5" style={{ display: 'flex' }}>
+      <div className="px-4" style={{ width: '34%' }}>
+        
+        <Table bordered>
+          <tbody>
+            
+          </tbody>
+
+        </Table>
+      </div>
+      <div className="px-4" style={{ width: '33%' }}>
+        <h2>My Rockets</h2>
+        <Table bordered>
+          <tbody>
+            {/*     render here your list */}
+          </tbody>
+
+        </Table>
+      </div>
+      <div className="px-4" style={{ width: '33%' }}>
+        <h2>My Dragons</h2>
+        <Table bordered>
+          <tbody>
+            {/*     render here your list */}
+
+          </tbody>
+
+        </Table>
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
