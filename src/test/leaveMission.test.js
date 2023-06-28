570347562c214dafb01cff9802ba630f4877c771
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-28 20:49:11 +0300

    Add test cases for leaving misson function

--- src/test/leaveMission.test.js
+++ src/test/leaveMission.test.js
@@ -5,22 +5,22 @@ describe('leaveMission reducer', () => {
     const initialState = {
       missions: [
         {
-          mission_id: '454d', mission_name: 'Mission 1', description: 'Description 1', reserved: true,
+          mission_id: '4453', mission_name: 'Mission 1', description: 'Description 1', reserved: true,
         },
-        { mission_id: '532', mission_name: 'Mission 2', description: 'Description 2' },
+        { mission_id: '123', mission_name: 'Mission 2', description: 'Description 2' },
       ],
     };
 
     const expectedState = {
       missions: [
         {
-          mission_id: '454d', mission_name: 'Mission 1', description: 'Description 1', reserved: false,
+          mission_id: '4453', mission_name: 'Mission 1', description: 'Description 1', reserved: false,
         },
-        { mission_id: '532', mission_name: 'Mission 2', description: 'Description 2' },
+        { mission_id: '23', mission_name: 'Mission 2', description: 'Description 2' },
       ],
     };
 
-    const action = leavingMission({ id: '454d' });
+    const action = leavingMission({ id: '4453' });
     const actualState = missionsReducer(initialState, action);
 
     expect(actualState).toEqual(expectedState);
