cf4554467d1fdc03bc85b76f1731cd28a2d2743e
Nahid1911 <nahid.raihan@gmail.com>
2023-06-28 17:13:30 +0600

    Add cancel reserve functionality

--- src/components/Dragon/DragonItem.js
+++ src/components/Dragon/DragonItem.js
@@ -3,47 +3,54 @@ import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import './Dragon.css';
 import { useDispatch } from 'react-redux';
-import { reserveDragon } from '../../redux/dragonsSlice/dragonsSlice';
+import { reserveDragon, cancelReservation } from '../../redux/dragonsSlice/dragonsSlice';
 
 const DragonItem = ({
   id, name, type, description, flickrImages,
 }) => {
   const dispatch = useDispatch();
   const [reserved, setReserved] = useState(false);
+  const [btnClass, setBtnClass] = useState('fbg-primary text-white');
 
   const handleReserveDragon = () => {
-    dispatch(reserveDragon(id));
-    setReserved(true);
+    if (reserved) {
+      dispatch(cancelReservation(id));
+      setReserved(false);
+      setBtnClass('fbg-primary text-white');
+    } else {
+      dispatch(reserveDragon(id));
+      setReserved(true);
+      setBtnClass('bg-light text-danger');
+    }
   };
 
   return (
-    <Container className="mainCardContainer">
+    <div className="container">
       <div className="dragonImgContainre">
         <Image className="dragonImg" src={flickrImages} />
       </div>
       <div className="dragonInfoContainer">
         <Card.Body key={type}>
           <Card.Title>{name}</Card.Title>
-
           <Card.Text className="descrStyle">
             {reserved && <span className="reserveSpan">| Reserved | </span>}
             {description}
           </Card.Text>
           <Button
+            className={btnClass}
             id={id}
             variant="primary"
             onClick={handleReserveDragon}
-            disabled={reserved} // Disable button when already reserved
           >
-            {reserved ? 'Reserved' : 'Reserve Dragon'}
+            {reserved ? 'Cancel Reservation' : 'Reserve Dragon'}
           </Button>
         </Card.Body>
       </div>
-    </Container>
+    </div>
   );
 };
 
